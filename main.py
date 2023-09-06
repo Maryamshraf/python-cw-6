3bf788f9fa88d8d4b6c13a96794301f63f21d0ce
Maryamshraf <142664253+Maryamshraf@users.noreply.github.com>
2023-09-06 19:39:48 +0300

    9

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# write your code here
+person={
+    "name":"maryam",
+    "age" : 18,
+    "hobbies":["swimming","writing","watching films"]
+}
+print(person["name"])
+print(person["age"])
+person["age"]=15
+print(person["age"])
+person["country"]= "japan"
+print(person)
+print(len(person))
+person["hobbies"].append("coding")
+print(person["hobbies"])
+
+def check_hobbies(person):
+    if len(person["hobbies"])>=3:
+        print("you are amazing!")
+    else:
+        print("think harder")
+check_hobbies(person)    
+ 
